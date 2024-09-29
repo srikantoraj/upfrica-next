a2abe13939f4d52da69b8fd60109383172f18738
Srikanto Rajbongshi <srikantoraj123@gmail.com>
2024-09-29 14:02:46 +0600

    changed

--- src/app/(pages)/details/[id]/page.jsx
+++ src/app/(pages)/details/[id]/page.jsx
@@ -1,8 +1,7 @@
-// app/details/[id]/page.jsx
 
 import React from 'react';
+import Head from "next/head";
 
-// Fetch product data based on ID
 async function getProductData(id) {
   const res = await fetch(`https://upfrica-staging.herokuapp.com/api/v1/products/${id}`);
 
@@ -25,20 +24,23 @@ export async function generateStaticParams() {
 }
 
 export default async function ProductDetails({ params }) {
-  // Get the product ID from the params
   const { id } = params;
-
-  // Fetch product data using the ID
   const product = await getProductData(id);
 
-  return (
+    return (
+      <>
+        <Head>
+        <title>{product.title} - Product Details</title>
+        <meta name="description" content={product.description} />
+        </Head>
+            
     <div className="container mx-auto px-4 py-10 bg-white">
       <h1 className="text-xl md:text-2xl font-extrabold tracking-wide">
         {product.title}
       </h1>
-      {/* <img src={product.image} alt={product.name} className="w-full h-auto" /> */}
-      {/* <p>{product.description}</p> */}
-      {/* <p className="text-lg font-semibold">Price: {product.price} USD</p> */}
+
     </div>
+      </>
+   
   );
 }
