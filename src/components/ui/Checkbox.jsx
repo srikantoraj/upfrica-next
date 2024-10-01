b024084b7b085ee568bd28494da03a9f18c965bb
bappy <bappymodok0011@gmail.com>
2024-10-01 13:23:29 +0600

    edit page some modify

--- src/components/ui/Checkbox.jsx
+++ src/components/ui/Checkbox.jsx
@@ -1,13 +1,13 @@
 const Checkbox = ({ label, id }) => (
-    <div className="flex w-full mb-4">
+    <div className=" w-full mb-4 grid grid-cols-3 md:grid-cols-5">
       <label
-        className="text-[#747579] w-2/6 text-left pr-4 text-base font-bold"
+        className="text-[#747579]   pr-4 text-base font-bold col-span-1 md:col-span-1 text-right"
         htmlFor={id}
       >
         {label}
       </label>
       <input
-        className="border border-gray-300 rounded focus:outline-none"
+        className="border border-gray-300 rounded focus:outline-none col-span-1 h-4 md:col-span-1"
         type="checkbox"
         id={id}
       />
--- src/components/ui/FormInput.jsx
+++ src/components/ui/FormInput.jsx
@@ -1,13 +1,13 @@
 const FormInput = ({ label, type, id, placeholder }) => (
-    <div className="flex w-full mb-4">
+    <div className="grid grid-cols-3 md:grid-cols-5">
       <label
-        className="text-[#747579] md:w-3/6 text-left pr-4 text-base font-bold"
+        className="text-[#747579]  pr-4 text-base font-bold col-span-1 md:col-span-1 text-right"
         htmlFor={id}
       >
         {label}
       </label>
       <input
-        className="w-3/4 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-base"
+        className=" p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-base col-span-2 md:col-span-3"
         type={type}
         id={id}
         placeholder={placeholder}
