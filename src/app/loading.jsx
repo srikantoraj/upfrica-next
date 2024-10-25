95da17deb574f0e2169f20cadcae72392e471b4d
Bappy Modok <bappymodok0011@gmail.com>
2024-10-25 09:17:56 +0600

    some change

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const loading = () => {
     return (
-        <div>
+        <div className="h-screen flex justify-center  items-center">
             <Loading/>
         </div>
     );
