19b586e297a884be5226a866d3561f4f078d0bc5
Srikanto Rajbongshi <srikantoraj123@gmail.com>
2025-04-25 16:35:42 +0600

    url updated

---
+++ src/app/(pages)/dashboard/payment-success/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PaymentSuccess() {
+  return (
+      <div>
+          <h2>Your payment is successful</h2>
+          <p>thanks for your payment</p>
+    </div>
+  )
+}
+
+export default PaymentSuccess
